421cef50ce20d786a26e98796e1451577bf69c61
Ruan Brandão <ruan.bernardo@gmail.com>
2023-08-01 19:25:42 -0300

    Setup player authentication

--- lib/pongx_web/router.ex
+++ lib/pongx_web/router.ex
@@ -1,6 +1,8 @@
 defmodule PongxWeb.Router do
   use PongxWeb, :router
 
+  import PongxWeb.PlayerAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule PongxWeb.Router do
     plug :put_root_layout, html: {PongxWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_player
   end
 
   pipeline :api do
@@ -39,6 +42,44 @@ defmodule PongxWeb.Router do
 
       live_dashboard "/dashboard", metrics: PongxWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", PongxWeb do
+    pipe_through [:browser, :redirect_if_player_is_authenticated]
+
+    live_session :redirect_if_player_is_authenticated,
+      on_mount: [{PongxWeb.PlayerAuth, :redirect_if_player_is_authenticated}] do
+      live "/players/register", PlayerRegistrationLive, :new
+      live "/players/log_in", PlayerLoginLive, :new
+      live "/players/reset_password", PlayerForgotPasswordLive, :new
+      live "/players/reset_password/:token", PlayerResetPasswordLive, :edit
+    end
+
+    post "/players/log_in", PlayerSessionController, :create
+  end
+
+  scope "/", PongxWeb do
+    pipe_through [:browser, :require_authenticated_player]
+
+    live_session :require_authenticated_player,
+      on_mount: [{PongxWeb.PlayerAuth, :ensure_authenticated}] do
+      live "/players/settings", PlayerSettingsLive, :edit
+      live "/players/settings/confirm_email/:token", PlayerSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", PongxWeb do
+    pipe_through [:browser]
+
+    delete "/players/log_out", PlayerSessionController, :delete
+
+    live_session :current_player,
+      on_mount: [{PongxWeb.PlayerAuth, :mount_current_player}] do
+      live "/players/confirm/:token", PlayerConfirmationLive, :edit
+      live "/players/confirm", PlayerConfirmationInstructionsLive, :new
     end
   end
 end
